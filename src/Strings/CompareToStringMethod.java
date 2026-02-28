ce8bae0c6d556496bcfb759a6d9d2387b8665df7
Mainak Biswas <mainak@Mac.hsd1.ca.comcast.net>
2026-02-27 17:19:26 -0800

    Added comareTostringMethod

--- src/Strings/CompareToStringMethod.java
+++ src/Strings/CompareToStringMethod.java
@@ -2,5 +2,10 @@ package Strings;
 //package Java_dsa_interview_prep;
 
 public class CompareToStringMethod {
-    
+    public static void main(String[] args) {
+        String a = "Satyarth";
+        String b = "Jennia";
+        System.out.println(a.compareTo(b));  //compareTo() method compares two strings lexicographically and returns an integer value based on the comparison
+
+    }
 }
